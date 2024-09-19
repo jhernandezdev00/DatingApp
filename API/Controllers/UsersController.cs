3586f6b1ca797e92160e5322bb282ec1dbd95d17
GeorgeDX2 <jorgeanto2302@gmail.com>
2024-09-18 19:24:09 -0600

    Parcial_01 - Add ApiControllerBase

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,10 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/v1/[controller]")]
-
-public class UsersController:ControllerBase{
+public class UsersController:BaseApiController{
     private readonly DataContext _context;
 
     public UsersController(DataContext context)
