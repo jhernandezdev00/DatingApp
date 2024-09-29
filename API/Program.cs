ed60d96df435de8b40830eff8b87d597717ebb0f
GeorgeDX2 <jorgeanto2302@gmail.com>
2024-09-28 18:46:36 -0600

    Parcial_01 - Add extension methods

--- API/Program.cs
+++ API/Program.cs
@@ -1,40 +1,21 @@
 using System.Text;
 using API.Data;
+using API.Extensions;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaulConnection"));
-});
-
-builder.Services.AddCors();
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => 
-    {
-        var tokenKey = builder.Configuration["TokenKey"] ?? throw new ArgumentNullException("TokenKey");
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
-
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
-//Configure the HTTP request pipeline.
-app.UseCors(cors => cors.AllowAnyHeader().AllowAnyMethod().WithOrigins(
-        "http://localhost:4200","https://localhost:4200"));
 
+//Configure the HTTP request pipeline.
+app.UseCors(cors => cors.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 app.Run();
