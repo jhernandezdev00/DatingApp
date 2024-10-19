5752c462f338158dec247b801803b7e82714701b
Jorge Hernández <123610586+jhernandezdev00@users.noreply.github.com>
2024-10-19 12:44:19 -0600

    Parcial01 - Code For First Evaluation (#1)
    Squash and Merge:
    
    * Configuracion Inicial Parcial_01
    
    * Configuracion De Los Archivos
    
    * Parcial_01 - Primer Entities
    
    * Parcial_01 - Agregado EntityFramework Core
    
    * Parcial_01 - Agregado DataContext
    
    * Parcial_01 - Se agrego el primer EF Migration
    
    * Parcial_01 - Se agraga base de datos usando dotnet ef database comand
    
    * Parcial_01 - Agregar Usuarios al Controlador
    
    * Parcial_01 - Agrega Funciones Asincronas En El Codigo
    
    * Parcial_01 - Modificacion En .gitignore
    
    * Initial Angular Commit
    
    * Parcial_01 - Add First Angular Component
    
    * Parcial_01 -Add Users List to HTML
    
    * Parcial_01 - Add ngx-bootstrap@12 bootstrap font-awesome
    
    * Parcial_01- Add Password to AppUser
    
    * Parcial_01 - Add ApiControllerBase
    
    * Parcial_01 Add DTOs
    
    * Parcial_01 - Add Login Endpoint
    
    * Parcial_01 - Add Token Service
    
    * Parcial_01 - Add Token Services v2
    
    * Parcial_01 - Add auth middleware
    
    * Parcial_01 - Add change to gitignore
    
    * Parcial_01 - Add extension methods
    
    * Parcial_01 - Add navbar to angular SPA
    
    * Parcial_01 - Add  Angular Template Forms
    
    * Parcial_01 - Inject Account Service Into Component
    
    * Parcial_01 - Add conditionls to show/remove content from DOM
    
    * Parcial_01 - Add Bootstrap dropdown
    
    * Parcial_01 - Persist the login
    
    * Parcial_01 - Add home page
    
    * Parcial_01 - Add register form
    
    * Parcial_01 - CHILD to PARENT communication
    
    * Parcial_01 - CHILD to PARENT communication v1
    
    * Parcial_01 - Add User Registration

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,37 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { RegisterComponent } from "../register/register.component";
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RegisterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit{
+  registerMode = false;
+  http = inject(HttpClient);
+  users: any;
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  registerToggle(): void{
+    this.registerMode = !this.registerMode;
+  }
+
+  cancelRegisterMode(event: boolean): void{
+    this.registerMode = event;
+  }
+
+  getUsers(){
+    this.http.get("https://localhost:5001/api/v1/users").subscribe({
+      next: (response) => { this.users = response; },
+      error: (error) => { console.log(error); },
+      complete: () => { console.log("Request Completed!"); }
+    });
+  }
+
+}
