4a9b927a9f79486bf59bef83805bc84c9968a536
GeorgeDX2 <jorgeanto2302@gmail.com>
2024-10-19 16:12:06 -0600

    Parcial_02 - Add buggy controller to test errors

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,24 @@
+using API.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController (DataContext context) : BaseApiController{
+    [Authorize]
+    [HttpGet("auth")]
+    public ActionResult<string> GetAuth() => "Secret Text";
+
+    [HttpGet("not-found")]
+    public ActionResult<string> GetNotFound() => NotFound();
+
+    [HttpGet("server-error")]
+    public ActionResult<string> GetServerError(){
+        var result = context.Users.Find(-1) ?? 
+            throw new Exception ("Server Error occured!");
+        return "Random Text";
+    }
+
+    [HttpGet("bad-request")]
+    public ActionResult<string> GetBadRequest() => BadRequest("Bad Request Happened");
+}
