973657a5e5bbd728bb3f514aa6f90eb127a45a04
GeorgeDX2 <jorgeanto2302@gmail.com>
2024-10-19 18:00:15 -0600

    Parcial_02 - Add new props to AppUser and create DateTime extension method

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -18,21 +18,22 @@ public class AccountController(DataContext context, ITokenService tokenService) 
             return BadRequest("Nombre de Usuario En Uso :(");
         }
 
-        using var hmac = new HMACSHA512();
+        return Ok();
+        //using var hmac = new HMACSHA512();
 
-        var user = new AppUser{
-            UserName = request.UserName,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password)),
-            PasswordSalt = hmac.Key
-        };
+        // var user = new AppUser{
+        //     UserName = request.UserName,
+        //     PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password)),
+        //     PasswordSalt = hmac.Key
+        // };
 
-        context.Users.Add(user);
-        await context.SaveChangesAsync();
+        // context.Users.Add(user);
+        // await context.SaveChangesAsync();
 
-        return new UserResponse{
-            Username = user.UserName,
-            Token = tokenService.CreateToken(user)
-        };
+        // return new UserResponse{
+        //     Username = user.UserName,
+        //     Token = tokenService.CreateToken(user)
+        // };
     }
 
     [HttpPost("login")]
