d7327a0a223c3ac292ce32710d45ff99ad396dac
GeorgeDX2 <jorgeanto2302@gmail.com>
2024-10-26 17:39:52 -0600

    Parcial_02 - Configure AutoMapper

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -5,7 +5,11 @@ namespace API.Helpers;
 
 public class AutoMapperProfiles : Profile{
     public AutoMapperProfiles(){
-        CreateMap<AppUser,MemberResponse>();
+        CreateMap<AppUser, MemberResponse>().ForMember(
+            d => d.PhotoUrl, 
+            o => o.MapFrom(
+                s => s.Photos.FirstOrDefault(
+                    p => p.IsMain)!.Url));
         CreateMap<Photo, PhotoResponse>();
     }
 }
