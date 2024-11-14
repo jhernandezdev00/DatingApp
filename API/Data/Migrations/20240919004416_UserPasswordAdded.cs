3cebe6557d7fa998c4e9dca53584e9d122789aa2
GeorgeDX2 <jorgeanto2302@gmail.com>
2024-11-14 17:47:01 -0600

    Parcial_03 -  Update tests

--- API/Data/Migrations/20240919004416_UserPasswordAdded.cs
+++ API/Data/Migrations/20240919004416_UserPasswordAdded.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -7,6 +8,7 @@ namespace API.Data.Migrations
     /// <inheritdoc />
     public partial class UserPasswordAdded : Migration
     {
+        [ExcludeFromCodeCoverage]
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
