a1ea09e97d677713c5ab005049be7477f06b24ee
george <jorgeanto2302@gmail.com>
2025-04-19 14:55:23 -0600

    Parcial_05 - Optimize action filter

--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -1,12 +1,23 @@
+using System.Globalization;
 using System.Security.Claims;
 
 namespace API.Extensions{
     public static class ClaimsPrincipleExtensions{
-        public static string GetUserName(this ClaimsPrincipal user){
-            
-            var username = user.FindFirstValue(ClaimTypes.NameIdentifier) 
+        
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
+            var userId = int.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier)
+                ?? throw new ArgumentException("Cannot get the userId from token"), CultureInfo.InvariantCulture);
+
+            return userId;
+        }
+
+        public static string GetUserName(this ClaimsPrincipal user)
+        {
+
+            var username = user.FindFirstValue(ClaimTypes.Name)
                 ?? throw new ArgumentException("No se puede obtener el nombre de usuario del token");
-            
+
             return username;
         }
     }
