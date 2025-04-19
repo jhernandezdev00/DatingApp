34ee6084782e81c6f487b41a9014cce0009cf540
george <jorgeanto2302@gmail.com>
2025-04-19 16:05:51 -0600

    Parcial_05 - Add Like feature in SPA

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,6 +1,7 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, computed, inject, input, ViewEncapsulation } from '@angular/core';
 import { Member } from '../../_models/member';
 import { RouterLink } from '@angular/router';
+import { LikesService } from '../../_services/likes.service';
 
 @Component({
   selector: 'app-member-card',
@@ -12,5 +13,7 @@ import { RouterLink } from '@angular/router';
 })
 
 export class MemberCardComponent {
+  private likesService = inject(LikesService);
   member = input.required<Member>();
+  hasLiked = computed(() => this.likesService.likeIds().includes(this.member().id));
 }
