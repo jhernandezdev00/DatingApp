3b2737c49168eb8129d8eb7300f1f79c7bd8f1d8
george <jorgeanto2302@gmail.com>
2025-04-19 23:26:04 -0600

    Parcial_06 - Configure IdentityDbContext

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,17 +1,31 @@
+namespace API.Data;
 using API.DataEntities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Data;
-
-public class DataContext(DbContextOptions options) : DbContext(options)
+public class DataContext(DbContextOptions options) : IdentityDbContext<
+        AppUser, AppRole, int, IdentityUserClaim<int>, AppUserRole,
+        IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>(options)
 {
-    public DbSet<AppUser> Users { get; set; }
     public DbSet<UserLike> Likes { get; set; }
     public DbSet<Message> Messages { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+
+        builder.Entity<AppUser>()
+            .HasMany(ur => ur.UserRoles)
+            .WithOne(u => u.User)
+            .HasForeignKey(ur => ur.UserId)
+            .IsRequired();
+
+        builder.Entity<AppRole>()
+            .HasMany(ur => ur.UserRoles)
+            .WithOne(u => u.Role)
+            .HasForeignKey(ur => ur.RoleId)
+            .IsRequired();
 
         builder.Entity<UserLike>()
             .HasKey(k => new { k.SourceUserId, k.TargetUserId });
@@ -27,7 +41,7 @@ public class DataContext(DbContextOptions options) : DbContext(options)
             .WithMany(l => l.LikedByUsers)
             .HasForeignKey(f => f.TargetUserId)
             .OnDelete(DeleteBehavior.Cascade);
-            
+
         builder.Entity<Message>()
             .HasOne(s => s.Recipient)
             .WithMany(m => m.MessagesRecieved)
