0a7c9fe254f57c75af9236382132460571f1e7fb
george <jorgeanto2302@gmail.com>
2025-04-19 21:44:08 -0600

    Parcial_05 - Fix to messageS/thread, in PLURAL, see messageS service line 27

--- client/src/app/_services/messages.service.ts
+++ client/src/app/_services/messages.service.ts
@@ -16,7 +16,7 @@ export class MessagesService {
 
   getMessages(pageNumber: number, pageSize: number, container: string) {
     let params = setPaginationHeaders(pageNumber, pageSize);
-    params = params.append("Container", container);
+    params = params.append("Container", container.toLocaleLowerCase());
 
     return this.http.get<Message[]>(this.baseUrl + "messages",
       { observe: "response", params }).subscribe({
@@ -25,7 +25,7 @@ export class MessagesService {
   }
 
   getMessageThread(username: string) {
-    return this.http.get<Message[]>(this.baseUrl + "message/thread/" + username);
+    return this.http.get<Message[]>(this.baseUrl + "messages/thread/" + username);
   }
 
 }
