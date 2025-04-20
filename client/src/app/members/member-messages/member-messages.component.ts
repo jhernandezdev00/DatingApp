a01e1c00fa0a63404d1a408aa766cab26b793c5b
george <jorgeanto2302@gmail.com>
2025-04-19 22:07:31 -0600

    Parcial_05 - Send messages

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject, input, OnInit } from '@angular/core';
+import { Component, inject, input, OnInit, output, ViewChild } from '@angular/core';
 import { MessagesService } from '../../_services/messages.service';
 import { Message } from '../../_models/message';
 import { TimeagoModule } from 'ngx-timeago';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-messages',
@@ -10,12 +11,21 @@ import { TimeagoModule } from 'ngx-timeago';
   templateUrl: './member-messages.component.html',
   styleUrl: './member-messages.component.css'
 })
-export class MemberMessagesComponent implements OnInit {
+export class MemberMessagesComponent {
+  @ViewChild("messageForm") messageForm?: NgForm;
   private messagesService = inject(MessagesService);
   username = input.required<string>();
   messages = input.required<Message[]>();
 
-  ngOnInit(): void {
-    
+  messageContent = "";
+  updateMessages = output<Message>();
+
+  sendMessage() {
+    this.messagesService.sendMessage(this.username(), this.messageContent).subscribe({
+      next: message => {
+        this.updateMessages.emit(message);
+        this.messageForm?.reset();
+      }
+    });
   }
 }
