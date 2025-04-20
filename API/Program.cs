c901ab3516fbc5ebbde36200c7a1b47212cfbda3
george <jorgeanto2302@gmail.com>
2025-04-19 23:33:47 -0600

    Parcial_06 - Update the seed method

--- API/Program.cs
+++ API/Program.cs
@@ -1,14 +1,18 @@
 namespace API;
 
 using API.Data;
+using API.DataEntities;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 
+[ExcludeFromCodeCoverage]
 public class Program
 {
     public static async Task Main(string[] args)
@@ -20,11 +24,11 @@ public class Program
         try
         {
             var context = services.GetRequiredService<DataContext>();
-            // var userManager = services.GetRequiredService<UserManager<AppUser>>();
+            var userManager = services.GetRequiredService<UserManager<AppUser>>();
             // var roleManger = services.GetRequiredService<RoleManager<AppRole>>();
 
             await context.Database.MigrateAsync();
-            await Seed.SeedUsersAsync(context); // (userManager, roleManger);
+            await Seed.SeedUsersAsync(userManager); // (userManager, roleManger);
         }
         catch (Exception ex)
         {
