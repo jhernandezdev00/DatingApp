98a442acc32b7280348f300530e3255d10156f75
george <jorgeanto2302@gmail.com>
2025-06-12 17:39:11 -0600

    Parcial_06 - Edit roles

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,21 +1,62 @@
 namespace API.Controllers;
 
+using API.DataEntities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-public class AdminController : BaseApiController
+public class AdminController(UserManager<AppUser> userManager) : BaseApiController
 {
     [Authorize(Policy = "RequireAdminRole")]
     [HttpGet("users-with-roles")]
-    public ActionResult GetUsersWithRole()
+    public async Task<ActionResult> GetUsersWithRole()
     {
-        return Ok("ACCESO ADMINISTRADOR!");
+        var users = await userManager.Users
+            .OrderBy(x => x.UserName)
+            .Select(x => new
+            {
+                x.Id,
+                Username = x.UserName,
+                Roles = x.UserRoles.Select(r => r.Role.Name).ToList()
+            }).ToListAsync();
+
+        return Ok(users);
+    }
+
+    [Authorize(Policy = "RequireAdminRole")]
+    [HttpPost("{username}")]
+    public async Task<ActionResult> EditRoles(string username, string roles)
+    {
+        if (string.IsNullOrEmpty(roles))
+        {
+            return BadRequest("Debe proporcionar al menos un rol");
+        }
+
+        var selectedRoles = roles.Split(",").ToArray();
+        var user = await userManager.FindByNameAsync(username);
+
+        if (user == null)
+        {
+            return BadRequest("No existe el usuario");
+        }
+
+        var userRoles = await userManager.GetRolesAsync(user);
+        var result = await userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+        if (!result.Succeeded) return BadRequest("Fallo en agregar el rol");
+
+        result = await userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+        if (!result.Succeeded) return BadRequest("Fallo al eliminar el rol");
+
+        return Ok(await userManager.GetRolesAsync(user));
     }
 
     [Authorize(Policy = "ModeratePhotoRole")]
     [HttpGet("photos-to-moderate")]
     public ActionResult GetPhotosForModeration()
     {
-        return Ok("ACCESO MODERADOR!");
+        return Ok("Moderator access granted!");
     }
 }
