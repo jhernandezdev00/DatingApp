a84d3912e0973c01030efc40761d21874ad9d524
george <jorgeanto2302@gmail.com>
2025-06-13 18:31:56 -0600

    Parcial_06 - Add send message to Message Hub

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,7 +40,7 @@ public class Startup{
         app.UseEndpoints(endpoints => {
             endpoints.MapControllers();
             endpoints.MapHub<PresenceHub>("hubs/presence");
-            endpoints.MapHub<MessageHub>("hubs/message");
+            endpoints.MapHub<MessageHub>("hubs/messages");
         });
     }
 }
