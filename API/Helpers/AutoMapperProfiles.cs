0de5bd1bf6e88a346abfbfd3377d8976615d9f78
george <jorgeanto2302@gmail.com>
2025-06-13 19:58:36 -0600

    Parcial_06 - Add UTC date formats

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -1,10 +1,10 @@
+namespace API.Helpers;
+
+using System.Globalization;
 using API.DataEntities;
 using API.DTOs;
 using API.Extensions;
 using AutoMapper;
-using System.Globalization;
-
-namespace API.Helpers;
 
 public class AutoMapperProfiles : Profile
 {
@@ -19,10 +19,14 @@ public class AutoMapperProfiles : Profile
         CreateMap<MemberUpdateRequest, AppUser>();
         CreateMap<RegisterRequest, AppUser>();
         CreateMap<string, DateOnly>().ConvertUsing(s => DateOnly.Parse(s, CultureInfo.InvariantCulture));
-         CreateMap<Message, MessageResponse>()
+        CreateMap<Message, MessageResponse>()
             .ForMember(d => d.SenderPhotoUrl,
                 o => o.MapFrom(s => s.Sender.Photos.FirstOrDefault(p => p.IsMain)!.Url))
             .ForMember(d => d.RecipientPhotoUrl,
                 o => o.MapFrom(s => s.Recipient.Photos.FirstOrDefault(p => p.IsMain)!.Url));
+        CreateMap<DateTime, DateTime>()
+            .ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
+        CreateMap<DateTime?, DateTime?>()
+            .ConvertUsing(d => d.HasValue ? DateTime.SpecifyKind(d.Value, DateTimeKind.Utc) : null);
     }
 }
